321293059c2bfa05e03936c32183d3865bf02eb6
Pavan0229 <bugganapavan@gmail.com>
2022-08-14 21:37:14 +0530

    added JWT TOken

---
+++ src/main/java/com/example/model/JwtRequest.java
@@ -0,0 +1,39 @@
+package com.example.model;
+
+public class JwtRequest {
+
+	String userName;
+	String password;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public JwtRequest(String userName, String password) {
+		super();
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public JwtRequest() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "JwtRequest [userName=" + userName + ", password=" + password + "]";
+	}
+
+}
